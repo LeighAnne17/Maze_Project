4210916b6405aad36d0e096c52ffc1b5ffc86823
LeighAnne17 <leighndimande17@icloud.com>
2024-09-19 14:57:51 -0700

    updates

--- project/inc/raycasting.h
+++ project/inc/raycasting.h
@@ -2,12 +2,13 @@
 #define RAYCASTING_H
 
 #include <SDL2/SDL.h>
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
-#define MAP_WIDTH 8
-#define MAP_HEIGHT 7
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define MAP_WIDTH 10
+#define MAP_HEIGHT 8
 
-void render_scene(SDL_Renderer *renderer, float posX, float posY, float dirX, float dirY, float planeX, float planeY);
+void render_scene(SDL_Renderer *renderer, float posX, float posY, float dirX, float dirY, float planeX, float planeY, char worldMap[MAP_WIDTH][MAP_HEIGHT]);
+int load_map(const char *filename, char worldMap[MAP_WIDTH][MAP_HEIGHT]);
 
 #endif /* RAYCASTING_H */
 
